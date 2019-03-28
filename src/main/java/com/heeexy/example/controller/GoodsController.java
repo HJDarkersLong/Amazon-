8c4f6f3abf5019028ca50a775a7f9e5591f50e41
HJDarkersLong <hujun5112@gmail.com>
2019-03-28 18:55:37 +0800

    --hujun--商品添加创建人等信息

--- src/main/java/com/heeexy/example/controller/GoodsController.java
+++ src/main/java/com/heeexy/example/controller/GoodsController.java
@@ -2,6 +2,7 @@ package com.heeexy.example.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.heeexy.example.service.GoodsService;
+import com.heeexy.example.service.LoginService;
 import com.heeexy.example.util.CommonUtil;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
+    @Autowired
+    private LoginService loginService;
+
     /**
      * 查询商品列表
      */
@@ -33,18 +37,24 @@ public class GoodsController {
     @RequiresPermissions("goods:add")
     @PostMapping("/addGoods")
     public JSONObject addGoods (@RequestBody JSONObject requestJson) {
-        requestJson.put("create_by","admin");
+        JSONObject info = loginService.getInfo();
+        System.out.println(info);
         requestJson.put("create_date",new Timestamp(new Date().getTime()));
         requestJson.put("update_date",new Timestamp(new Date().getTime()));
-        requestJson.put("update_by","admin");
         requestJson.put("delete_status","1");
-        CommonUtil.hasAllRequired(requestJson,
+        /*CommonUtil.hasAllRequired(requestJson,
                 "name,cn_name,en_name,pcl_no,sku_no," +
                         "other_name,cn_customs_name,en_customs_name,hs_code,category_no," +
                         "tag_no,brand_no,business_dev_user_no, buy_qus_user_no, buy_user_no," +
                         "length,width,height,weight,body_weight_5000,body_weight_6000,base_price," +
                         "sale_price,pic_address,description,easy_discription,key_code,status," +
-                        "create_by,create_date,update_by, update_date,remarks");
+                        "create_by,create_date,update_by, update_date,remarks");*/
+       /* CommonUtil.hasAllRequired(requestJson,
+                "name,pcl_no,sku_no," +
+                        "hs_code,category_no," +
+                        "er_no, buy_user_no," +
+                        "length,width,height,weight,body_weight_5000,body_weight_6000,base_price," +
+                        "sale_price,description,easy_discription,status,");*/
         return goodsService.addGoods(requestJson);
     }
 }
