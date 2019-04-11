b152a1a8440834034cff1c319db628b54ec15122
HJ <hujun5112@gmail.com>
2019-04-12 03:39:52 +0800

    --hujun--测试家里电脑后台提交代码

--- src/main/java/com/heeexy/example/config/CorsConfig.java
+++ src/main/java/com/heeexy/example/config/CorsConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 @Configuration
 public class CorsConfig {
@@ -21,5 +26,21 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig()); // 4
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // 限制了路径和域名的访问
+                /*registry.addMapping("/api*").allowedOrigins("http://localhost:8081");*/
+                registry.addMapping("/**")
+                        .allowedOrigins(ALL)
+                        .allowedMethods(ALL)
+                        .allowedHeaders(ALL)
+                        .allowCredentials(true);
+            }
+        };
     }
 }
--- src/main/java/com/heeexy/example/config/shiro/ShiroConfiguration.java
+++ src/main/java/com/heeexy/example/config/shiro/ShiroConfiguration.java
@@ -46,7 +46,7 @@ public class ShiroConfiguration {
 		filterChainDefinitionMap.put("/login/auth", "anon");
 		filterChainDefinitionMap.put("/login/logout", "anon");
 		filterChainDefinitionMap.put("/error", "anon");
-		filterChainDefinitionMap.put("/**", "authc");
+		filterChainDefinitionMap.put("/**", "anon");
 		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
 		return shiroFilterFactoryBean;
 	}
